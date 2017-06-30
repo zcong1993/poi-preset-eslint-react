40ff2e1ed9839f1cd13758c3d8884e4dbd8fcffe
zcong1993 <1005997919@qq.com>
2017-06-30 14:24:22 +0800

    finished

--- index.js
+++ index.js
@@ -4,7 +4,21 @@ const eslintFormatter = require('eslint-formatter-pretty')
 const baseOptions = {
   formatter: eslintFormatter,
   baseConfig: {
-    extends: ['xo', 'xo-react']
+    extends: ['xo', 'xo-react'],
+    env: {
+      browser: true,
+      commonjs: true,
+      es6: true,
+      jest: true,
+      node: true,
+    }
+  },
+  rules: {
+    'semi': ['error', 'never'],
+    'indent': ['error', 2, { "SwitchCase": 2 }],
+    'object-curly-spacing': ['error', 'always'],
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-indent': ['error', 2]
   },
   useEslintrc: false
 }
